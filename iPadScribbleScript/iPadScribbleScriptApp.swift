9e65908a5f88d258c999030d62e659d7b676bb14
Kelvin <63611619+kelvinjou@users.noreply.github.com>
2025-01-11 14:51:02 -0800

    Initial Commit

---
+++ iPadScribbleScript/iPadScribbleScriptApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPadScribbleScriptApp.swift
+//  iPadScribbleScript
+//
+//  Created by Kelvin J on 1/11/25.
+//
+
+import SwiftUI
+
+@main
+struct iPadScribbleScriptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
